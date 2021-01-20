cfc7db4a175774796a561ea32b5ddbe2a3d6ea26
FrozenPriest <boral777@mail.ru>
2021-01-20 11:29:30 +0300

    Not, equal conditionals

---
+++ app/src/main/java/ru/frozenpriest/taskautomaton/program/commands/variables/EqualVar.kt
@@ -0,0 +1,13 @@
+package ru.frozenpriest.taskautomaton.program.commands.variables
+
+import android.content.Context
+import ru.frozenpriest.taskautomaton.program.Conditional
+import ru.frozenpriest.taskautomaton.program.Program
+
+class EqualVar(val var1: String, val var2: String) : Conditional() {
+    override fun perform(program: Program, context: Context) {
+        val var1Value = program.variables[var1].toString()
+        val var2Value = program.variables[var2].toString()
+        conditionResult = var1Value == var2Value
+    }
+}
---
+++ app/src/main/java/ru/frozenpriest/taskautomaton/program/commands/variables/NotConditional.kt
@@ -0,0 +1,12 @@
+package ru.frozenpriest.taskautomaton.program.commands.variables
+
+import android.content.Context
+import ru.frozenpriest.taskautomaton.program.Conditional
+import ru.frozenpriest.taskautomaton.program.Program
+
+class NotConditional(val conditional: Conditional) : Conditional() {
+    override fun perform(program: Program, context: Context) {
+        conditional.perform(program, context)
+        conditionResult = !conditional.conditionResult
+    }
+}
